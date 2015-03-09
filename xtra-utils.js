00102a816b907150f8b17662059eaaf25d3026cd
Suman Lama <suman@zyoba.com>
2015-03-09 16:16:42 +0545

    Adds getting a unique element function and counting the unique elements function

--- xtra-utils.js
+++ xtra-utils.js
@@ -1,11 +1,12 @@
 'use strict';
 
 var xtra_utils = {
-    /*sort sorts an array. it can sort an array of objects with a reference.*/
-    sort: function(order, sorter) {
+    /*compare sorts an array. it can sort an array of objects with a reference.*/
+    compare: function(order, sorter) {
         /*. order = 1 for ascending or order = 0 for descending*/
         switch (order){
             case 0 :
+                /*returns a compare function that tells sort function to sort in descending order*/
                 return function (a, b) {
                     a = a[sorter] ? a[sorter] : a;
                     b = b[sorter] ? b[sorter] : b;
@@ -17,6 +18,7 @@ var xtra_utils = {
                 };
 
             case 1 :
+                /*returns a compare function that tells sort function to sort in ascending order*/
                 return function (a, b) {
                     a = a[sorter] ? a[sorter] : a;
                     b = b[sorter] ? b[sorter] : b;
@@ -29,6 +31,7 @@ var xtra_utils = {
                 
             default :
                 return function (a, b) {
+                    /*returns a compare function that tells sort function to sort in ascending order*/
                     a = a[sorter] ? a[sorter] : a;
                     b = b[sorter] ? b[sorter] : b;
                     if (a < b)
@@ -44,16 +47,35 @@ var xtra_utils = {
         /*uses RFC2822 regex pattern for email validation*/
         var pattern = /[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
         return pattern.test(email);
+    },
+
+    getUniqueElements: function(array) {
+        var uniqueElm = {};
+        array.forEach(function(entry) {
+            uniqueElm[entry] = 1;
+        });
+
+        return Object.keys(uniqueElm);
+    },
+
+    countUnique: function(array) {
+        var uniqueElm = {}, count = 0;
+        array.forEach(function(entry) {
+                uniqueElm[entry] = ++uniqueElm[entry] || 1;
+        });
+        return uniqueElm;
     }
 };
 
+module.exports = xtra_utils;
 
 
 /*-------------------------------Test Codes----------------------------------------------*/
 if(require.main === module) {
     var arr = [{ count: 1}, {count: 5}, {count: 3}];
-    var arr1 = [1,2,5,3];
-    arr.sort(xtra_utils.sort(1, 'count'));
+    var arr1 = [1,2,5,3,2];
+    arr.sort(xtra_utils.compare(1, 'count'));
     console.log(arr);
-
+    console.log(xtra_utils.getUniqueElements(arr1));
+    console.log(xtra_utils.countUnique(arr1));
 }
